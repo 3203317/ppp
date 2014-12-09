2e7d48e390eace4d4af5c6eab0df1d1ac5c3bb2f
unknown <3203317@qq.com>
2014-12-09 15:36:44 +0800

    haha

--- cppt/src/main/java/com/xcysoft/cppt/dao/UserDao.java
+++ cppt/src/main/java/com/xcysoft/cppt/dao/UserDao.java
@@ -1,13 +1,22 @@
 package com.xcysoft.cppt.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
 import com.xcysoft.cppt.dao.base.BaseUserDao;
 import com.xcysoft.cppt.model.User;
+
+import framework.core.utils.pagesUtils.Page;
 
 /**
  *
  * @author huangxin
  *
  */
+@Repository
+@SuppressWarnings("all")
 public class UserDao extends BaseUserDao {
 
 	/**
@@ -17,6 +26,16 @@ public class UserDao extends BaseUserDao {
 	 */
 	public User getByName(String name) {
 		return null;
+	}
+
+	public Page findAll(Map<String, Object> paramMap) {
+		Map<String, Object> param = new HashMap<String, Object>();
+		StringBuilder sql = new StringBuilder(
+				"SELECT * FROM S_USER t WHERE 1=1");
+		if (null != paramMap) {
+			// TODO
+		}
+		return this.sqlqueryForpage(sql.toString(), param, null);
 	}
 
 }
--- cppt/src/test/java/com/xcysoft/cppt/dao/TestUserDao.java
+++ cppt/src/test/java/com/xcysoft/cppt/dao/TestUserDao.java
@@ -1,10 +1,18 @@
 package com.xcysoft.cppt.dao;
 
+import junit.framework.TestCase;
+
 /**
  *
  * @author huangxin
  *
  */
-public class TestUserDao {
+public class TestUserDao extends TestCase {
 
+	private UserDao userDao;
+
+	public void testFindAll() {
+		userDao = new UserDao();
+		userDao.findAll(null);
+	}
 }
