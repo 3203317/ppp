ada2f06422201b8e3113738b6b2139e20d506d0c
unknown <3203317@qq.com>
2014-12-14 19:49:59 +0800

    haha

--- foundation-cppt/src/main/java/com/xcysoft/foundation/controller/UserController.java
+++ foundation-cppt/src/main/java/com/xcysoft/foundation/controller/UserController.java
@@ -1,10 +1,24 @@
 package com.xcysoft.foundation.controller;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.xcysoft.framework.core.handler.MultiViewResource;
+
 /**
  *
  * @author huangxin
  *
  */
-public class UserController {
+@Controller
+@Scope("prototype")
+@RequestMapping(value = "/user")
+@SuppressWarnings("all")
+public class UserController extends MultiViewResource {
 
+	@RequestMapping(value = "addUserUI")
+	public String addUserUI() {
+		return getUrl("user.addUserUI");
+	}
 }
