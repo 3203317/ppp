a6fe3e036da90cd83c35c22108afdde5615603d6
unknown <3203317@qq.com>
2015-01-08 11:19:43 +0800

    haha

--- foundation-cppt/src/main/java/com/xcysoft/foundation/cppt/controller/SiteController.java
+++ foundation-cppt/src/main/java/com/xcysoft/foundation/cppt/controller/SiteController.java
@@ -1,5 +1,10 @@
 package com.xcysoft.foundation.cppt.controller;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -9,6 +14,11 @@ import org.springframework.web.servlet.ModelAndView;
 
 import cn.newcapec.framework.core.handler.MultiViewResource;
 import cn.newcapec.framework.core.utils.fileUtils.SysConfigUtil;
+import cn.newcapec.framework.core.utils.pagesUtils.Page;
+import cn.newcapec.framework.core.utils.pagesUtils.PageContext;
+import cn.newcapec.framework.core.utils.pagesUtils.PageView;
+
+import com.xcysoft.foundation.cppt.biz.MenuService;
 
 /**
  *
@@ -21,6 +31,11 @@ import cn.newcapec.framework.core.utils.fileUtils.SysConfigUtil;
 @SuppressWarnings("all")
 public class SiteController extends MultiViewResource {
 
+	@Autowired
+	private MenuService menuService;
+
+	private LinkedHashMap<String, String> orderby = new LinkedHashMap<String, String>();
+
 	/**
 	 *
 	 * @param modelMap
@@ -30,6 +45,24 @@ public class SiteController extends MultiViewResource {
 	public ModelAndView indexUI(ModelMap modelMap) {
 		modelMap.put("cdn", SysConfigUtil.get("html.cdn"));
 		modelMap.put("virtualPath", SysConfigUtil.get("html.virtualPath"));
+
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("pid", "00");
+		PageContext.setPageSize(Integer.MAX_VALUE);
+
+		orderby.put("sort", "asc");
+		Page page = menuService.findList(paramMap, orderby);
+		PageView<Map<String, Object>> pageView = new PageView<Map<String, Object>>(
+				PageContext.getPageSize(), PageContext.getOffset());
+		pageView.setQueryResult(page);
+		modelMap.put("menuView", pageView);
+
+		page = getChildMenus();
+		pageView = new PageView<Map<String, Object>>(PageContext.getPageSize(),
+				PageContext.getOffset());
+		pageView.setQueryResult(page);
+		modelMap.put("childMenusView", pageView);
+
 		return toView(getUrl("manage.indexUI"), modelMap);
 	}
 
@@ -38,5 +71,25 @@ public class SiteController extends MultiViewResource {
 		modelMap.put("cdn", SysConfigUtil.get("html.cdn"));
 		modelMap.put("virtualPath", SysConfigUtil.get("html.virtualPath"));
 		return toView(getUrl("manage.welcomeUI"), modelMap);
+	}
+
+	/**
+	 * 获取二级子菜单
+	 *
+	 * @return
+	 */
+	private Page getChildMenus() {
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("pid", getFirstMenuId());
+		Page page = menuService.findList(paramMap, orderby);
+		return page;
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	private String getFirstMenuId() {
+		return "02";
 	}
 }
