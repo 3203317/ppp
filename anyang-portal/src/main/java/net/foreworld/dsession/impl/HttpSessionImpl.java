f87238d83ffdcca267dd02350bfd6f7b7ff27ae6
unknown <3203317@qq.com>
2016-01-18 23:21:17 +0800

    haha

--- anyang-portal/src/main/java/net/foreworld/dsession/impl/HttpSessionImpl.java
+++ anyang-portal/src/main/java/net/foreworld/dsession/impl/HttpSessionImpl.java
@@ -1,11 +1,7 @@
 package net.foreworld.dsession.impl;
 
-import javax.servlet.http.HttpServletRequest;
-
 import net.foreworld.dsession.HttpSession;
-
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
+import net.foreworld.dsession.utils.HttpUtil;
 
 /**
  *
@@ -14,30 +10,18 @@ import org.springframework.web.context.request.ServletRequestAttributes;
  */
 public class HttpSessionImpl implements HttpSession {
 
-	public void setAttribute(String name, Object val) {
-		javax.servlet.http.HttpSession session1 = getSession();
-		session1.setAttribute(name, val);
+	public void setAttribute(String name, Object value) {
+		javax.servlet.http.HttpSession session = HttpUtil.getSession();
+		session.setAttribute(name, value);
 	}
 
 	public void invalidate() {
-		javax.servlet.http.HttpSession session1 = getSession();
-		session1.invalidate();
+		javax.servlet.http.HttpSession session = HttpUtil.getSession();
+		session.invalidate();
 	}
 
 	public Object getAttribute(String name) {
-		javax.servlet.http.HttpSession session1 = getSession();
-		return session1.getAttribute(name);
-	}
-
-	public static javax.servlet.http.HttpSession getSession() {
-		javax.servlet.http.HttpSession session = null;
-		session = getRequest().getSession();
-		return session;
-	}
-
-	public static HttpServletRequest getRequest() {
-		ServletRequestAttributes attrs = (ServletRequestAttributes) RequestContextHolder
-				.getRequestAttributes();
-		return attrs.getRequest();
+		javax.servlet.http.HttpSession session = HttpUtil.getSession();
+		return session.getAttribute(name);
 	}
 }
