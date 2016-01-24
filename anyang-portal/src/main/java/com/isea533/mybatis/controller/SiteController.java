ef290745a78ed6ee298d2d130acc5e61df1d17e6
unknown <3203317@qq.com>
2016-01-24 15:50:01 +0800

    haha

--- anyang-portal/src/main/java/com/isea533/mybatis/controller/SiteController.java
+++ anyang-portal/src/main/java/com/isea533/mybatis/controller/SiteController.java
@@ -1,11 +1,12 @@
 package com.isea533.mybatis.controller;
 
+import net.foreworld.dsession.DistributedSession;
+import net.foreworld.dsession.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.isea533.mybatis.model.User;
 
 /**
  *
@@ -18,14 +19,11 @@ public class SiteController {
 	private String index_ftl = "site/1.0.1/index";
 
 	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
-	public ModelAndView indexUI() {
+	public ModelAndView indexUI(@DistributedSession HttpSession session) {
 		ModelAndView result = new ModelAndView(index_ftl);
 		// TODO
-		User user = new User();
-		user.setUserName("admin");
-		user.setUserPass("123456");
-		// TODO
-		result.addObject("data_session_user", user);
+		Object obj = session.getAttribute("session.user");
+		result.addObject("data_session_user", obj);
 		return result;
 	}
 }
