76b7415b921f9e4acd06b3839d8a387fca0fe65e
unknown <3203317@qq.com>
2016-02-23 22:57:31 +0800

    haha

--- framework-util/src/main/java/net/foreworld/util/DateUtil.java
+++ framework-util/src/main/java/net/foreworld/util/DateUtil.java
@@ -2,6 +2,7 @@ package net.foreworld.util;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -13,22 +14,72 @@ import java.util.Date;
  */
 public class DateUtil {
 
+	public static String DATE_PATTERN = "yyyy-MM-dd";
+	public static String DATETIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+	public static String TIME_PATTERN = "HH:mm:ss";
+
+	private static SimpleDateFormat dateFormat = new SimpleDateFormat(
+			DATE_PATTERN);
+
 	/**
 	 * 检查字符串是否是日期格式
 	 *
 	 * @param format
-	 *            "yyyy-MM-dd"
+	 *            默认 "yyyy-MM-dd"
 	 * @param str
 	 *            "2007-02-28"
 	 * @return
 	 */
-	public static boolean checkDateFormat(String format, String str) {
-		final DateFormat df = new SimpleDateFormat(format);
+	public static Date checkDateFormat(String format, String str) {
+		str = StringUtil.isEmpty(str);
+		if (null == str)
+			return null;
+
+		// TODO
+		DateFormat df = (null == format) ? dateFormat : new SimpleDateFormat(
+				format);
+
 		try {
 			Date date = (Date) df.parse(str);
-			return str.equals(df.format(date));
+			return (str.equals(df.format(date))) ? date : null;
 		} catch (Exception ignore) {
-			return false;
+			return null;
 		}
+	}
+
+	/**
+	 * 日期类型转换字符串
+	 *
+	 * @param format
+	 *            默认 "yyyy-MM-dd"
+	 * @param date
+	 * @return
+	 */
+	public static String date2Str(String format, Date date) {
+		DateFormat df = (null == format) ? dateFormat : new SimpleDateFormat(
+				format);
+		return df.format(date);
+	}
+
+	/**
+	 * 获取相对日期
+	 *
+	 * @param date
+	 * @param field
+	 *            Calendar.DATE
+	 * @param amount
+	 *            -5
+	 * @return
+	 */
+	public static Date getRelativeDate(Date date, int field, int amount) {
+		if (null == date)
+			return null;
+
+		Calendar newDate;
+		(newDate = Calendar.getInstance()).setTime(date);
+
+		// TODO
+		newDate.add(field, amount);
+		return newDate.getTime();
 	}
 }
